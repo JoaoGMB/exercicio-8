549e11b4889a94100db279c45bed6754e3d890ed
JoaoGMB <muller.bordin@gmail.com>
2026-03-24 16:42:20 -0300

    Add files via upload

---
+++ DepartamentoApplication.java
@@ -0,0 +1,12 @@
+
+package com.exemplo.departamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DepartamentoApplication.class, args);
+    }
+}
